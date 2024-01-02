1ec6924bc966aa4ddb84f325c903d3136aafec21
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-02 16:56:35 +0300

    Add victory message after all letters are guessed

--- master.py
+++ master.py
@@ -25,33 +25,35 @@ def check_letter(word, guessed_letters, letter):
        return False
 
 def play_game():
-  words = load_words()
-  word = choose_word(words)
-  guessed_letters = {}
-  missed_letters = []
-  displayed_word = "_" * len(word)
+ words = load_words()
+ word = choose_word(words)
+ guessed_letters = {}
+ missed_letters = []
+ displayed_word = "_" * len(word)
 
-  while len(missed_letters) < 6:
-      display_hangman(missed_letters)
-      print("Current word: ", displayed_word)
-      guess = input("Guess a letter: ")
-      if not guess.isalpha():
-          print("Please enter a letter.")
-          continue
-      if guess in guessed_letters or guess in missed_letters:
-          print("You have already guessed this letter.")
-          continue
-      if check_letter(word, guessed_letters, guess):
-          guessed_letters[guess] = True
-          for i in range(len(word)):
-              if word[i] == guess:
-                 displayed_word = displayed_word[:i] + guess + displayed_word[i+1:]
-      else:
-          missed_letters.append(guess)
+ while len(missed_letters) < 6:
+     display_hangman(missed_letters)
+     print("Current word: ", displayed_word)
+     guess = input("Guess a letter: ")
+     if not guess.isalpha():
+         print("Please enter a letter.")
+         continue
+     if guess in guessed_letters or guess in missed_letters:
+         print("You have already guessed this letter.")
+         continue
+     if check_letter(word, guessed_letters, guess):
+         guessed_letters[guess] = True
+         for i in range(len(word)):
+             if word[i] == guess:
+                displayed_word = displayed_word[:i] + guess + displayed_word[i+1:]
+     else:
+         missed_letters.append(guess)
 
-  if len(missed_letters) == 6:
-      print("You lost! The word was %s" % word)
-  else:
-      print("You won! The word was %s" % word)
+     if displayed_word == word:
+         print("You won! The word was %s" % word)
+         break
+
+ if len(missed_letters) == 6:
+     print("You lost! The word was %s" % word)
 
 play_game()
